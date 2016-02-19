17c83561086e81b319dfad7f4a90b943ee67c650
Christopher Dobler <chrisdobler@gmail.com>
2016-02-19 03:56:31 -0800

    Camera View Controller Updates

--- app/controllers/camera_controller.rb
+++ app/controllers/camera_controller.rb
@@ -1,6 +1,8 @@
 class CameraController < ApplicationController
 
 	def view
+		@device = Device.find_by! friendly_name: "camera1"
+		@stream_address = "http://" + @device.ipv4_address + ":8080"
 	end
 
 	def settings
